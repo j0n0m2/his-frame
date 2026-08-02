7a581afb263f19ac96331437aae336356517c6eb
j0n0m2 <dbsk8223@naver.com>
2026-08-02 10:17:05 +0900

    fix : style 수정 및 스크롤 위치 초기화를 위한 ScrollRestoration 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import Navbar from "./components/Navbar"; // Navbar 위치에 맞게 수정
 import "./styles/App.scss";
 
 function App() {
   return (
-    <div className="app-container">
+    <>
       <Navbar />
       <main className="main-content">
         <Outlet />
       </main>
-    </div>
+      <ScrollRestoration />
+    </>
   );
 }
 
