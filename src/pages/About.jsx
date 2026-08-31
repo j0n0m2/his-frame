7c11ff470a955599bb6713d48d185bd1f3a8aba9
j0n0m2 <dbsk8223@naver.com>
2026-08-31 14:15:48 +0900

    feat : About 페이지 사진 및 정보 작성

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,14 @@ function About() {
   return (
     <div>
       <h1>About</h1>
-      <p>This is the about page of our application.</p>
-      <div style={{ width: "100px", height: "1000px" }}></div>
+      <img src="/assets/his_frame_profile.webp" alt="작가사진" />
+      <p>
+        하나님께서 지으신 것들이 가장 아름답게 빛나는 순간들을 담아내고자
+        합니다.
+      </p>
+      <p>연락처 : dbsk8223@naver.com</p>
+      <p>카메라 : sony a6000</p>
+      <p>렌즈 : sony 16-50mm 표준 줌렌즈</p>
     </div>
   );
 }
